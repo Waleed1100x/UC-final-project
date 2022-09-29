86e3720f1947f66357ac9f218f6b65de38a019ea
Waleed1100x <107954669+Waleed1100x@users.noreply.github.com>
2022-09-29 15:43:08 +0300

    Done

--- flutter_application_project/lib/screens/home_screen.dart
+++ flutter_application_project/lib/screens/home_screen.dart
@@ -2,13 +2,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_project/screens/wilks_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
 const HomeScreen({ Key? key }) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context){
     return Scaffold(
-      backgroundColor: Colors.black12,
+      backgroundColor: Colors.grey,
      appBar: AppBar(
       title: Text("Home Page"),backgroundColor: Color.fromARGB(255, 18, 108, 21),
       ),
@@ -18,19 +23,15 @@ const HomeScreen({ Key? key }) : super(key: key);
          Row(
           mainAxisAlignment: MainAxisAlignment.center,
            children: [
-             Text("Welcome To POWERFITNESS ",style: TextStyle(color: Color.fromARGB(255, 18, 108, 21),fontSize: 25,fontWeight: FontWeight.bold),),
+             Text("Welcome To Calories Calculator ",style: TextStyle(color: Color.fromARGB(255, 18, 108, 21),fontSize: 25,fontWeight: FontWeight.bold),),
            ],
          ),
          Row(
             mainAxisAlignment: MainAxisAlignment.center,
            children: [
-             Text("Choose an option:",style: TextStyle(color: Color.fromARGB(255, 18, 108, 21),fontSize: 25,fontWeight: FontWeight.bold),),
+             Text("Press the button to start:",style: TextStyle(color: Color.fromARGB(255, 18, 108, 21),fontSize: 25,fontWeight: FontWeight.bold),),
            ],
          ),
-      ElevatedButton(onPressed: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) =>  WilksScreen(),));
-      },style: ElevatedButton.styleFrom(backgroundColor: Color.fromARGB(255, 18, 108, 21)), child: Text("Wilks Calculator")
-      ),
       ElevatedButton(onPressed: () {
          Navigator.push(context, MaterialPageRoute(builder: (context) =>  WilksScreen(),));
       },style: ElevatedButton.styleFrom(backgroundColor: Color.fromARGB(255, 18, 108, 21)), child: Text("Calorie Calculator")
--- flutter_application_project/lib/screens/wilks_screen.dart
+++ flutter_application_project/lib/screens/wilks_screen.dart
@@ -7,30 +7,144 @@ WilksScreen({ Key? key }) : super(key: key);
   State<WilksScreen> createState() => _WilksScreenState();
 }
 class _WilksScreenState extends State<WilksScreen> {
+String ? gender = "" ;
+  String dropdownvalue = 'Sedentary (office job)';  
+  var items = [   
+    'Sedentary (office job)',
+    'light Exercise (1-2 days)',
+    'Modreate Exercise (3-5 days)',
+    'Heavy Exercise (6-7 days)',
+    'Athlete (2x per day)',
+  ];
+  final age = TextEditingController();
+  final weight = TextEditingController();
+  final height = TextEditingController();
+  double Totalcalories = 0 ; 
+int currentindex = 0;
   @override
   Widget build(BuildContext context){
-String gender = "Male";
+
     return Container(
       child: Scaffold(
+          backgroundColor: Colors.grey,
         appBar: AppBar(
       title: Text("Home Page"),backgroundColor: Color.fromARGB(255, 18, 108, 21),) ,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Row(),
-         RadioListTile(title: Text("Male"),value: "Male", groupValue: gender, onChanged: (value){setState(() {
-           gender = value.toString();
-         });}),
-         RadioListTile(title: Text("Female"),value: "Female", groupValue: gender, onChanged: (value){setState(() {
-           gender = value.toString();
-         });}),
-          Row(children: [Text("Age",style: TextStyle(fontSize: 18),),TextField(keyboardType: TextInputType.number,),]),
-          Row(children: [Text('Weight',style:TextStyle(fontSize: 18)),TextField(keyboardType: TextInputType.number,),],),
-          Row(children: [Text('Height',style: TextStyle(fontSize: 18),),TextField(keyboardType: TextInputType.number,),],),
+                const Text('gender:'),
+      Row(children: [
+        
+      ],),
+Row(
+  mainAxisAlignment: MainAxisAlignment.center,
+  
+  children: [
+    
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ElevatedButton(style: ElevatedButton.styleFrom(backgroundColor: Color.fromARGB(255, 18, 108, 21)),
+          onPressed: () {
+    
+      int index = 1 ; 
+    
+      changeIndex(index);
+    
+    }, child: Text('Male')),
+        ),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ElevatedButton(style: ElevatedButton.styleFrom(backgroundColor: Color.fromARGB(255, 18, 108, 21)),
+        onPressed: () {
+    
+        int index = 2 ; 
+    
+        changeIndex(index);
+    
+    },child: Text('Female'),
+        ),
+      )
+  ],
+),
+             Text("$gender "),
+             
+          Container(width: 150,child: TextField(controller: age,keyboardType: TextInputType.number,decoration: InputDecoration(hintText: "Age", ),)),
+        Container(width: 150,child: TextField(controller: weight,keyboardType: TextInputType.number,decoration: InputDecoration(hintText:"Weight in kg" ),)),
+          Container(width: 150,child: TextField(controller: height,keyboardType: TextInputType.number,decoration: InputDecoration(hintText: "Height in cm"),)),
+        DropdownButton(
+               
+              // Initial Value
+              value: dropdownvalue,
+               
+              // Down Arrow Icon
+              icon: const Icon(Icons.keyboard_arrow_down),   
+               
+              // Array list of items
+              items: items.map((String items) {
+                return DropdownMenuItem(
+                  value: items,
+                  child: Text(items),
+                );
+              }).toList(),
+              // After selecting the desired option,it will
+              // change button value to selected value
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownvalue = newValue!;
+                });
+              },
+            ),
+            ElevatedButton(style: ElevatedButton.styleFrom(backgroundColor: Color.fromARGB(255, 18, 108, 21))
+              ,onPressed: () {
+              calculator(age.text, weight.text, height.text, gender!,dropdownvalue);
+            }, child:Text('Calculate!')),
+      Divider(),
+       Text(style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),'Your Total Maintenance Calories is $Totalcalories'),
         ],
       ), 
       )
    
     );
   }
+  void changeIndex(int index){
+setState(() {
+ currentindex= index;
+ if(index == 1){
+  gender = 'Male';
+ }else
+ {
+  gender = 'Female';
+ }
+});
+  }
+  void calculator(String sAge, String sWeight, String sHeight,String gender, String activity){
+int age= int.parse(sAge);
+int weight = int.parse(sWeight);
+int height  = int.parse(sHeight);
+if(gender == 'Male'){
+Totalcalories = ((10*weight + 6.25*height - 5.0*age) + 5) ;
+}
+else {
+Totalcalories = ((10*weight + 6.25*height - 5.0*age) - 151) ;
+} 
+ if(activity =='Sedentary (office job)'){
+  Totalcalories += 358;
+ }
+    else if(activity == 'light Exercise (1-2 days)'){
+      Totalcalories += 671;
+    }
+   else  if(activity =='Modreate Exercise (3-5 days)'){
+      Totalcalories += 984;
+    }
+   else  if(activity == 'Heavy Exercise (6-7 days)'){
+      Totalcalories += 1297;
+    }
+   else if(activity =='Athlete (2x per day)'){
+      Totalcalories += 1610;
+    }
+    print(Totalcalories);
+setState(() {
+  
+});
+ }
 }
